a16c581c0b77efd67987859ce9c3ec114601ef87
liyuanjie <yuanjie.li@watrix.ai>
2023-01-29 14:55:01 +0800

    修改shll文件

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-# 忽略错误
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
+# 生成静态文件
 npm run docs:build
 
-# 进入待发布的目录
-cd docs/.vitepress/dist
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -16,9 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果部署到 https://<USERNAME>.github.io
-git push -f https://github.com/liyuanjie0225/liyuanjie0225.github.io.git master
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:liyuanjie0225/liyuanjie0225.github.io.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
+cd -
